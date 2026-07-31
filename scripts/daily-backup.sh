5ff834553f88cf2f8e0d5520c733cac625020da6
Gonçalo <g.j.teixeiradepinhoferreira@uva.nl>
2026-07-31 14:40:32 +0200

    Setting the backup server.

--- scripts/daily-backup.sh
+++ scripts/daily-backup.sh
@@ -2,9 +2,11 @@
 
 DUMP_DIR="dump"
 mkdir -p $DUMP_DIR
-NEW_FILE="$DUMP_DIR/$(date +%s)_grisc_sa.dump"
+NEW_DUMP_FILE="$DUMP_DIR/$(date +%s)_grisc_sa.dump"
 docker compose exec -T postgres \
      pg_dump -U postgres -d grisc_sa -Fc \
-     > $NEW_FILE
+     > $NEW_DUMP_FILE
 
-# scp $NEW_FILE backup-user@backup-server:/
+# scp $NEW_DUMP_FILE backup-user@backup-server:/
+set -a; source .env.production; set +a;
+scp "$NEW_DUMP_FILE" $SERVER_BACKUP_USER@$SERVER_BACKUP:/home/goncalo/grissa_backup/
